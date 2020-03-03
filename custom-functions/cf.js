bc6c57f983032eebfdb29dc73abebb9df211a438
Sean Ruhland <sean.ruhland@gmail.com>
2020-03-02 21:20:52 -0800

    function exp

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,6 +1,31 @@
-console.log('it works!')
+// console.log('it works!')
 
-function calcluateBill() {
-    //this is the function body
-    console.log('calculate bill')
+// //function definition
+// let bill = 100;
+// let taxRate = 0.13;
+
+// function calcluateBill(billAmount, taxRate) {
+//     //this is the function body
+//     console.log('running calculate bill')
+//     const total = billAmount * 1 + taxRate;
+//     return total;
+// }
+//  const myTotal2 = calcluateBill(200 + 30 +20, .13);
+// // console.log(myTotal, myTotal2);
+// console.log(myTotal2)
+// const wesTotal = 500;
+// const wesTaxRate = .3;
+
+// function sayHiTo(firstName) {
+//     return `Hello ${firstName}`
+// }
+
+// console.log(sayHiTo)
+
+function doctorize(name) {
+    return `Dr. ${name}`;
+}
+
+function yell(name) {
+    return `HEY ${name.toUpperCase()}`;
 }
